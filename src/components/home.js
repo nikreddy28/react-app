5d837f16f55fb99a5dd2c425d4db86764a8dddbd
Nikhila <nikreddy@deloitte.com>
2018-11-09 12:31:59 +0530

    styling changes

--- src/components/home.js
+++ src/components/home.js
@@ -9,67 +9,39 @@ export default ()=> {
                     <p className = "header" > OMNI FULFILLMENT </p>  
                 </div>
             
-                <div className="col-lg-6 background-blue right-border">
-                <Link to = "/shipment" className = "btn btn-primary margin-top-10"> shipment </Link>
-                 <p> Start Picking </p>
+                <div className="col-md-6 col-sm-6 col-xs-6 background-blue right-border flex">
+                    <div className="align-center">
+                    
+                        <Link to = "/shipment" className = "btn margin-10"> <i className="fa fa-truck"></i> </Link>
+                        <p> Start Picking </p>
+                     </div>
                 </div>
-                <div className = "col-lg-6 background-blue" > 
-                <Link to = "/shipment" className = "btn btn-primary margin-top-10"> shipment </Link>
-                    <p>Custom Picking</p>
-                </div>
-                <div className = "col-lg-6 background-green" > 
-                <Link to = "/shipment" className = "btn btn-primary margin-top-10"> shipment </Link>
-                    <p>Shipments Ready </p>
-                </div>
-                <div className = "col-lg-6 background-purple" > 
-                < Link to = "/shipment" className = "btn btn-primary margin-top-10"> shipment </Link>
-                <p>BOSS</p>
-                </div>
-                
-
-
-
-
-
-
-              {/*      <div className = "fullwidth" >
-                        <div className = "black" >
-                            <p className = "header" > OMNI FULFILLMENT </p> 
-                        </div>
-                    </div> 
-                    <div className = "col-lg-6 division" >
-                        <div className = "blue" >
-                        < Link to = "/shipment" className = "btn"> shipment </Link>
-                            <p className = "picking" > Start Picking </p> 
-                        </div> 
-                    </div> 
-                    < div className = "col-lg-6  division" >
-                        <div className = "lightblue" >
-                            <p className = "custom" > Custom Picking </p> 
-                        </div> 
-                   </div> 
-                    < div className = "col-lg-6 division" >
-                        <div className = "green" >
-                            <p className = "shipment" > Shipments Ready </p> 
-                        </div> 
-                   </div> 
-                   < div className = "col-lg-6 division" >
-                        <div className = "purple" >
-                            <p className = "boss" > BOSS </p> 
-                        </div> 
-                   </div>
-
-                   <div className = "fullwidth" >
-                        <div className = "yellow" >
-                            <p className = "bold" > ASSIGN HOLD LOCATION </p>
-
-                            <p className = "picking" > Containers Ready </p> 
-                        </div> 
+                <div className = "col-md-6 col-sm-6 col-xs-6 background-blue flex"> 
+                    <div className="align-center">
+                        <Link to = "/shipment" className = "btn margin-10"> <i className="fa fa-cube"></i> </Link>
+                        <p>Custom Picking</p>
                     </div>
-                    <div className = "gray" >
-                    <div >
-                            <p className = "bold" > Workload Summary </p> </div>
-                    </div> */}
+                </div>
+                <div className = "col-md-6 col-sm-6 col-xs-6 background-green flex" > 
+                    <div className="align-center">
+                        <Link to = "/shipment" className = "btn margin-10 green-icon"> <i className="fa fa-circle-o"></i> </Link>
+                        <p>Shipments Ready </p>
+                    </div>
+                </div>
+                <div className = "col-md-6 col-sm-6 col-xs-6 background-purple flex" > 
+                    <div className="align-center">
+                        <Link to = "/shipment" className = "btn margin-10 purple-icon"> <i className="fa fa-cubes"></i> </Link>
+                        <p className="font-weight-bold">BOSS</p>
+                    </div>
+                </div>
+
+                 <div className = "col-md-12 col-sm-12 col-xs-12 background-orange flex" > 
+                    <div className="align-center">
+                        <p className="font-weight-bold margin-10">ASSIGN HOLD LOCATION</p>
+                        <Link to = "/shipment" className = "btn margin-10 orange-icon"> <i className="fa fa-circle-o"></i> </Link>
+                        <p>Container Ready</p>
+                    </div>
+                </div>
             </div>        
 
         )
--- src/components/shipment.js
+++ src/components/shipment.js
@@ -5,10 +5,13 @@ import { Link } from 'react-router';
 
 
 export default () => {
+    window.dataLayer.pageName = "shipment page";
         return ( 
-            <div className="col-lg-12">
+        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 flex-container">
+            <div className="row text-center"> 
                 <h1> Welcome to Shipment </h1>
                 <Link to = "/" className = "btn"> Home </Link>
             </div>
+        </div>
         )
     }
